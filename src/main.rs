252416dcf71238ed333c36e3d7f5296ce81fe87b
mewtek <mewtek@icloud.com>
2023-07-30 02:38:06 -0700

    Beginnings of install argument

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod api;
+mod install;
 
 use std::env;
 
@@ -12,15 +13,36 @@ async fn main() {
     {
         search(args[2].to_owned()).await;
     }
+
+    if args[1] == "install"
+    {
+        install(args[2].to_owned()).await;
+    }
 }
 
 /// Function for the "Search" argument
 async fn search(pkg_name: String)
 {
-    let results : Vec<api::SearchResults> = api::search_packages_loose(pkg_name).await;
+    let results : Vec<api::SearchResult> = api::search_packages_loose(pkg_name).await;
 
     for i in results
     {
         println!("{} {}{}{}\n:: {} | {}", ":::".green(), i.repo.red(), "/".green(), i.pkgname.blue(), i.pkgdesc, i.pkgver.yellow());
     } 
+}
+
+async fn install(pkg_name: String)
+{
+    let pkg : api::SearchResult = api::search_packages_exact(pkg_name).await;
+
+    println!("{} {}{}{}", ":::".bold().green(), pkg.repo.red(), "/".green(), pkg.pkgname.blue());
+    println!("==> Compressed size: {}\n==> Installed Size: {}", pkg.compressed_size.to_string().red(), pkg.installed_size.to_string().red());
+    println!("{}", "Depends On:".bold().green());
+    
+    for d in pkg.depends
+    {
+        println!("{} {}", "::".bold().green(), d.blue());
+    }
+
+    println!("Do you want to continue with package installation? [Y/N]")
 }
