1eb6f913ec16fe7b2b047ab9c31a02df3f997754
mewtek <mewtek@icloud.com>
2023-07-31 19:01:13 -0700

    get_config function

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,28 @@
 use std::fs;
+use serde_derive::Deserialize;
+use toml;
+
+#[derive(Debug, Deserialize)]
+pub struct Config
+{
+    pub general : General
+}
+
+#[derive(Debug, Deserialize)]
+pub struct General
+{
+    pub arch: String,
+    pub db_path: String,
+}
+
+pub fn get_config() -> Config
+{
+    let path = "/etc/meow.conf";
+    let contents = fs::read_to_string(path).expect("Failed to read contents of /etc/meow.conf.");
+    let config : Config = toml::from_str(&contents).unwrap();
+
+    return config;
+}
 
 /// Pulls mirrors from /etc/meow.d/mirrorlist
 pub fn get_mirrors() -> Vec<String>
@@ -19,4 +43,16 @@ pub fn get_mirrors() -> Vec<String>
     }
 
     return mirrors;
+}
+
+/// Checks the architecture of the CPU we're running the program on.
+pub fn get_cpu_arch() -> String
+{
+    let config = get_config();
+    if config.general.arch != "auto"
+    {
+        return config.general.arch;
+    }    
+
+    return "x86_64".to_string();
 }
