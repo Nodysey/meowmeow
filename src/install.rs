75776f8043be3c3bba98e58467df4596390aa4a8
mewtek <mewtek@icloud.com>
2023-08-01 19:42:16 -0700

    PackageDetails instead of package name

--- src/install.rs
+++ src/install.rs
@@ -10,11 +10,12 @@ use crate::config;
 use crate::database;
 
 /// Downloads a package from one of the mirrors in /etc/meow.d/mirrorlist
-/// TODO: This needs better error handling at some point.
-async fn download_pkg(pkg_name: String)
+async fn download_pkg(pkg: &api::PackageDetails)
 {
+    let config = config::get_config();
     let mirrors : Vec<String> = config::get_mirrors();
-    let arch = config::get_cpu_arch(); 
+    let download_path = config.general.download_path;
+    
     // TODO: Test mirror latency and determine the best one to download from
     let mirror : String = mirrors[1].to_owned();
     
@@ -24,46 +25,51 @@ async fn download_pkg(pkg_name: String)
             mirror, "$arch".yellow().bold(), "$repo".yellow().bold());
         return;
     }
-    
-    let package = api::search_packages_exact(pkg_name).await;
 
-    if Path::new(&format!("/tmp/meow/{}", package.filename)).exists()
+
+    if Path::new(&format!("{}{}", &download_path, pkg.filename)).exists()
     {
-        fs::remove_file(format!("/tmp/meow/{}", package.filename)).expect("Failed to remove file\nBad privileges?");
+        fs::remove_file(format!("{}{}", &download_path, pkg.filename)).expect("Failed to remove file\nBad privileges?");
     }
 
     let download_url = format!("{}/{}", 
-        mirror.replace("$arch", &arch.to_string()).replace("$repo", &package.repo.to_string()),
-        package.filename);
+        mirror.replace("$arch", &config.general.arch).replace("$repo", &pkg.repo.to_string()),
+        pkg.filename);
 
-    dbg!(&download_url);
-
-
+    // Download main archive
     let res = reqwest::get(&download_url).await.expect("WHOOPS!");
     let body = res.bytes().await.unwrap();
-    let mut out = File::create(format!("/tmp/meow/{}", package.filename)).expect("Failed to create file!");
+    let mut out = File::create(format!("{}{}", download_path, pkg.filename)).expect("Failed to create file!");
     out.write_all(&body).expect("Failed to write bytes!");
 
+    // TODO: Download archive signature
+    
 }
 
-/// Installs a package & its dependencies.
+/// Installs a pkg & its dependencies.
 pub async fn install_pkg(pkg_name: String)
 {
-    let package_details : api::PackageDetails = api::search_packages_exact(pkg_name).await;
-    let pkg_path = format!("/tmp/meow/{}", package_details.filename);
+    let config = config::get_config();
+    let package_details : api::PackageDetails = api::search_packages_exact(&pkg_name).await;
+    let pkg_path = format!("{}{}", config.general.download_path, &package_details.filename);
  
-    for dependency in package_details.depends
+    for dependency in &package_details.depends
     {
+        let x : api::PackageDetails = api::search_packages_exact(&dependency).await;
+        let depend_path = format!("{}{}", config.general.download_path, &x.filename);
         println!("{} Downloading {}..", "::".green().bold(), &dependency.to_string().blue());
-        download_pkg(dependency).await;
+        download_pkg(&x).await;
+        install_files(&depend_path);
+        database::add_pkg_to_database(&x).await;
     }
     
     println!("{} Downloading {}..", "::".green().bold(), &package_details.pkgname.to_string().blue());
-    download_pkg(package_details.pkgname).await;
-    install_files(pkg_path);
+    download_pkg(&package_details).await;
+    install_files(&pkg_path);
+    database::add_pkg_to_database(&package_details).await;
 }
 
-fn install_files(path: String)
+fn install_files(path: &str)
 {
     let path_compressed = &path;
     let path_decompressed = &path.replace(".tar.zst", ".tar");
@@ -72,7 +78,6 @@ fn install_files(path: String)
     expand_tar(path_decompressed);
 }
 
-// TODO: This should be async.
 /// Decompresses the .tar.zst file into a standard tar file for expansion into the main filesystem
 fn decompress_zstd(path: &str)
 {
