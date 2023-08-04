d0b2c16a0b5971ddd1d7ef1532d1ad44feeb880a
mewtek <mewtek@icloud.com>
2023-08-03 17:51:08 -0700

    Clean up main

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,12 @@ mod util;
 
 use std::env;
 
-use colored::Colorize;
-
-
 #[tokio::main]
 async fn main() {
     // TODO: Make an arc for the program config
 
     let args : Vec<String> = env::args().collect();
     
-
     if args.len() == 1
     {
         // TODO: Print help
@@ -28,43 +24,17 @@ async fn main() {
         return;
     }
 
-    if args[1] == String::from("search")
+    let operation : &str = &args[1].as_str();
+
+
+    match operation
     {
-        search(args[2].to_owned()).await;
+        "install"=>install::install(args[2].to_owned()).await,
+        "upgrade"=>println!("TODO"),
+        "upgrade-all"=>upgrade::upgrade_all().await,
+        "remove"=>println!("TODO"),
+        "search"=>user_util::search(args[2].to_owned()).await,
+        _=>println!("Invalid operation.\nType 'meow -h' for help.")
     }
 
-    if args[1] == "install"
-    {
-        install::install(args[2].to_owned()).await;
-    }
-
-    if args[1] == "installed-list" || args[1] == "packages"
-    {
-        for i in database::get_all_packages()
-        {
-            println!("{}", i);
-        }
-    }
-
-    if args[1] == "upgrade" || args[1] == "upgrade-all"
-    {
-        upgrade::upgrade_all().await;
-    }
-
-    if args[1] == "test" {
-        let mirror = &args[2];
-        user_util::add_mirror(mirror)
-    }   
  }
-
-/// Function for the "Search" argument
-async fn search(pkg_name: String)
-{
-    let results = api::search_packages_loose(&pkg_name).await
-        .expect("Package not found.");
-
-    for i in results
-    {
-        println!("{} {}{}{}\n:: {} | {}", ":::".green(), i.repo.red(), "/".green(), i.pkgname.blue(), i.pkgdesc, i.pkgver.yellow());
-    } 
-}
